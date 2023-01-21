e8fc1c47599b5901c67c921a2f35d82e9ea845df
lcwitte <122688319+lcwitte@users.noreply.github.com>
2023-01-21 16:02:24 +0100

    Custom_js folder added
    Custom_js folder is copy of website with validation on forms with custom js

---
+++ Custom_js/sign-up.js
@@ -0,0 +1,12 @@
+const fullName = document.getElementById('full-name-sign-up');
+const submitButton = document.getElementById('sign-up-button');
+const fullNameText = document.getElementById('full-name-text')
+
+submitButton.addEventListener('click', (event) => {
+  if(){
+    fullNameText.textContent = "hia";
+    fullNameText.style.color = "black"
+  }
+  
+  
+});
